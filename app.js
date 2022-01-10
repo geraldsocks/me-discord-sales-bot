fb41013a0b14c714c9d97bdce34979772a48305c
geraldsocks <93939339+geraldsocks@users.noreply.github.com>
2022-01-10 10:58:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,27 @@ const getListingMagicEden = (collection) => {
     });
 };
 
+const getDelistingMagicEden = (collection) => {
+    return new Promise((resolve) => {
+        const query = decodeURI(escape(JSON.stringify({
+            $match: {
+                collection_symbol: collection,
+                txType: 'cancelEscrow'
+            },
+            $sort: {
+                blockTime: -1
+            },
+            $skip: 0,
+            $limit: 10
+        })));
+        fetch(`https://api-mainnet.magiceden.io/rpc/getGlobalActivitiesByQuery?q=${query}`).then((res) => {
+            res.json().then((data) => {
+                resolve(data.results);
+            });
+        });
+    });
+}
+
 const getHistoryMagicEdenSales = (collection) => {
     return new Promise((resolve) => {
         const query = decodeURI(escape(JSON.stringify({
@@ -160,6 +181,94 @@ const synchronizeMagicEden = () => {
                 });
 
             });
+        
+        getDelistingMagicEden(collection).then((events) => {
+
+            console.log(`${new Date().toJSON()} Checking ${collectionName} Delistings`)
+
+            const sortedEvents = events
+                .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+
+            if (!sortedEvents.length) return;
+            
+            const newEvents = sortedEvents
+                .filter((e) => new Date(e.createdAt).getTime() > new Date(Date.now()).getTime() - process.env.INTERVAL_MILLISECONDS);
+
+            if (!newEvents.length) return;
+
+            newEvents.reverse().forEach(async (event) => {
+
+                //console.log(event)
+
+                if (!event.parsedUnlist) return;
+
+                const nft = await fetchMagicEdenNFT(event.parsedUnlist.mint);
+                
+                const attributes = nft.attributes;
+
+                attributes.forEach(attribute => {
+                    switch (attribute.trait_type) {
+                        case `Skin`:
+                            attrSkin = attribute.value;
+                        case `Shirt`:
+                            attrShirt = attribute.value;
+                        case `Hat`:
+                            attrHat = attribute.value;
+                    }
+                })
+
+                const tweetImg = `${nft.img}`;
+
+                switch (event.source) {
+                    case `magiceden`:
+                        sourceName = `on Magic Eden`;
+                        sourceURL = `https://magiceden.io/item-details/${event.mint}`
+                        tweetText = `${nft.title} just Delisted ${sourceName} ${sourceURL}`;
+                        console.log(`${tweetText} ${tweetImg}`);
+
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle(`${nft.title} just Delisted`)
+                            .setURL(`https://magiceden.io/item-details/${event.mint}`)
+                            // .addField('Price', `${(event.parsedTransaction.total_amount / 10E8).toFixed(2)} SOL`)
+                            // .addField('Buyer', event.parsedTransaction.buyer_address)
+                            // .addField('Seller', event.seller_address)
+                            .setThumbnail(nft.img)
+                            .setTimestamp(new Date(event.createdAt))
+                            .setColor('DARK_AQUA')
+                            .setFooter('Magic Eden')
+                            .addField('Skin', attrSkin, true)
+                            .addField('Shirt', attrShirt, true)
+                            .addField('Hat', attrHat, true);
+                            //.setThumbnail('');
+                            
+                        client.channels.cache.get(process.env.MAGICEDEN_LISTINGS_CHANNEL_ID).send({
+                            embeds: [embed]
+                        }).catch(() => {});
+                        //return formatAndSendTweet(tweetText,tweetImg);
+                        break;
+                    // case `alphaart`:
+                    //     sourceName = `on alpha art`;
+                    //     sourceURL = `https://alpha.art/t/${event.mint}`
+                    //     tweetText = `${nft.title} bought for ${(event.parsedTransaction.total_amount / 10E8).toFixed(2)} SOL ${sourceName} ${sourceURL}`;
+                    //     console.log(`${tweetText} ${tweetImg}`);
+                    //     //return formatAndSendTweet(tweetText,tweetImg);
+                    //     break;
+                    // case `digitaleyes`:
+                    //     sourceName = `on Digital Eyes`;
+                    //     sourceURL = `https://digitaleyes.market/item/SolSocks/${event.mint}`
+                    //     tweetText = `${nft.title} bought for ${(event.parsedTransaction.total_amount / 10E8).toFixed(2)} SOL ${sourceName} ${sourceURL}`;
+                    //     console.log(`${tweetText} ${tweetImg}`);
+                    //     //return formatAndSendTweet(tweetText,tweetImg);
+                    //     break;
+                    default:
+                        sourceName = ``
+                        sourceURL = ``
+                }
+                
+                });
+
+            });
+        
 
         getHistoryMagicEdenSales(collection).then((events) => {
 
